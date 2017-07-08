31854c8106516e18c47ce0fbc718da0840af16a1
AHarman <ah12819@my.bristol.ac.uk>
2017-07-08 22:18:06 +0100

    Moved oauth authentication into a decorator function

--- api.py
+++ api.py
@@ -2,7 +2,30 @@ import webapp2
 from oauth2client import client, crypt
 import logging
 import credentials
+import time
 
+def oauth_required(http_handler_function):
+	def inner(self):
+		id_token = self.request.body
+		try:
+			idinfo = client.verify_id_token(id_token, credentials.client_id)
+			if idinfo["iss"] not in ["accounts.google.com", "https://accounts.google.com"]:
+				raise crypt.AppIdentityError("Wrong issuer.")
+		except crypt.AppIdentityError:
+			self.response.write("Exception! Invalid token!")
+			self.response.status = "401"
+			return
+
+		current_time = int(time.time())
+		if not (int(idinfo["iat"]) < current_time and current_time < int(idinfo["exp"])):
+			self.response.write("Expired token! Need me a new one!")
+			self.response.status = "401"
+			return
+
+		userid = idinfo["sub"]
+		return http_handler_function(self)
+
+	return inner
 
 class Testing(webapp2.RequestHandler):
 	def get(self):
@@ -14,20 +37,12 @@ class Root(webapp2.RequestHandler):
 		self.response.write("This is a hardcoded string in python!")
 
 class Login(webapp2.RequestHandler):
+	@oauth_required
 	def post(self):
-		logging.debug("In the Login POST")
-
-		id_token = self.request.body
-		try:
-			idinfo = client.verify_id_token(id_token, credentials.client_id)
-			if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
-				raise crypt.AppIdentityError("Wrong issuer.")
-		except crypt.AppIdentityError:
-			self.response.write("Exception! Invalid token! Wrong Issuer!")
-
-		userid = idinfo['sub']
-		self.response.write("User id: " + userid)
-
+		self.response.write("You've been authenticated!")
+		return
+		
+		
 
 app = webapp2.WSGIApplication([
 	("/api/?", Root),
